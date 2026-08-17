84fe47c2826f8c5127629689cae4e40d2af786dd
Nicholas Lim <niclimcy@lineageos.org>
2026-08-18 02:40:40 +0800

    ErrorView: Add a refresh button
    Do not immediately redirect on error, show a refresh button instead.

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,12 +1,12 @@
 <template>
-  <main class="flex items-center justify-center text-center text-2xl">
-    <span>{{ store.error || message }}</span>
+  <main class="flex flex-col items-center justify-center text-center">
+    <span class="text-2xl">{{ store.error || message }}</span>
+    <button class="btn mt-2 px-4 py-1" @click="router.push('/')">Refresh</button>
   </main>
 </template>
 
 <script setup lang="ts">
 import { useUiStore } from '@/stores/ui'
-import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
 const store = useUiStore()
@@ -15,9 +15,5 @@ defineProps({
     type: String,
     default: 'An unknown error occurred'
   }
-})
-
-onMounted(async () => {
-  await router.push('/')
 })
 </script>
